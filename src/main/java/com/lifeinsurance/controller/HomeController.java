8d1d6230e6db8e799f77f60a356c81d1bfe5ccc7
hsenbassam <husseinbassam95@gmail.com>
2018-02-26 16:02:13 +0200

    Angular Page and Java run on the same server

--- src/main/java/com/lifeinsurance/controller/HomeController.java
+++ src/main/java/com/lifeinsurance/controller/HomeController.java
@@ -1,6 +1,11 @@
 package com.lifeinsurance.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,11 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public ModelAndView showHome() {
+	@RequestMapping(value = {"/", "/home", "/products", "/shopping-list", "/login", "/products"})
+	public String showHome() {
 		
-		ModelAndView modelAndView = new ModelAndView("index");
+		return "index.html";
 		
-		return modelAndView;
 	}
+	
+	
 }
--- src/main/java/com/lifeinsurance/controller/LoginController.java
+++ src/main/java/com/lifeinsurance/controller/LoginController.java
@@ -4,10 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lifeinsurance.model.Login;
@@ -29,24 +34,30 @@ public class LoginController {
 		return modelAndView;
 	}
 	
-	@RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
-	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("login") Login login) {
-		
-		ModelAndView modelAndView = null;
+//	@RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
+//	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("login") Login login) {
+//		
+//		ModelAndView modelAndView = null;
+//		
+//		User user = userService.validateUser(login);
+//		
+//		if(null != user) {
+//			modelAndView = new ModelAndView("welcome");
+//			modelAndView.addObject("firstname", user.getFirstname());
+//		}
+//		else {
+//			modelAndView = new ModelAndView("login");
+//			modelAndView.addObject("message", "Username or Password is Incorrect");
+//		}
+//		
+//		return modelAndView;
+//		
+//	}
+	@RequestMapping(value = "/loginProcess", method = RequestMethod.POST, consumes = {"application/json"})
+	public @ResponseBody User loginProcess(@RequestBody Login login) {	
 		
 		User user = userService.validateUser(login);
-		
-		if(null != user) {
-			modelAndView = new ModelAndView("welcome");
-			modelAndView.addObject("firstname", user.getFirstname());
-		}
-		else {
-			modelAndView = new ModelAndView("login");
-			modelAndView.addObject("message", "Username or Password is Incorrect");
-		}
-		
-		return modelAndView;
-		
+		return user;
 	}
 
 }
