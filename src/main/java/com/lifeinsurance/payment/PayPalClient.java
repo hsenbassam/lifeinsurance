8e0c9d991ccf68922f182e6e098b9fa0e69664c7
hsenbassam <husseinbassam95@gmail.com>
2018-03-28 17:10:13 +0300

    Adding Paypal Payment - No Client

---
+++ src/main/java/com/lifeinsurance/payment/PayPalClient.java
@@ -0,0 +1,94 @@
+package com.lifeinsurance.payment;
+
+
+import com.google.gson.Gson;
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PayPalClient {
+
+	// payment@lifeinsurance.com - Sandbox Account PayPal
+    String clientId = "AV3W_WxYt-h8irbEpFmQJ-I4urvlFkmpXtmxkCjC3CNCTwn915JaK8-Go6uPUyQdBlleDgCpIMkWUqNk";
+    String clientSecret = "EKHeoGQ0fBHltnDziXi887X6gGMi_5WiCpM43erfQnxtCs43BYkkn5udR-cydFwklSsTAqF5u6-yDbi-";
+
+
+    public Map<String, Object> createPayment(String sum){
+        Map<String, Object> response = new HashMap<String, Object>();
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(sum);
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:4200/payment/confirm");
+        redirectUrls.setReturnUrl("http://localhost:4200/payment/process");
+        payment.setRedirectUrls(redirectUrls);
+        Payment createdPayment;
+        try {
+            String redirectUrl = "";
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            createdPayment = payment.create(context);
+        
+            if(createdPayment!=null){
+                List<Links> links = createdPayment.getLinks();
+                for (Links link:links) {
+                    if(link.getRel().equals("approval_url")){
+                        redirectUrl = link.getHref();
+                        break;
+                    }
+                }
+                response.put("status", "success");
+                response.put("redirect_url", redirectUrl);
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println("Error happened during payment creation!");
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+    public Map<String, Object> completePayment(HttpServletRequest req){
+        Map<String, Object> response = new HashMap<String, Object>();
+        Payment payment = new Payment();
+        payment.setId(req.getParameter("paymentId"));
+        PaymentExecution paymentExecution = new PaymentExecution();
+        paymentExecution.setPayerId(req.getParameter("payerId"));
+        try {
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            Payment createdPayment = payment.execute(context, paymentExecution);
+            Gson gson = new Gson();
+        	String createdPaymentJson = gson.toJson(createdPayment);
+            if(createdPayment!=null){
+                response.put("status", "success");
+                response.put("payment", gson.fromJson(createdPaymentJson, Object.class));
+            }
+        } catch (PayPalRESTException e) {
+            System.err.println(e.getDetails());
+        }
+        return response;
+    }
+
+
+
+}
--- src/main/java/com/lifeinsurance/service/SimulatorServiceImpl.java
+++ src/main/java/com/lifeinsurance/service/SimulatorServiceImpl.java
@@ -2,8 +2,6 @@ package com.lifeinsurance.service;
 
 import com.lifeinsurance.model.Premium;
 import com.lifeinsurance.model.Quote;
-import com.lifeinsurance.model.TermLifePremium;
-import com.lifeinsurance.model.WholeLifePremium;
 import com.lifeinsurance.utils.TermLifePremiumCalculator;
 import com.lifeinsurance.utils.WholeLifePremiumCalculator;
 
@@ -12,7 +10,7 @@ public class SimulatorServiceImpl implements SimulatorService{
 	@Override
 	public Premium getTermLifePremium(Quote quote) {
 		
-		TermLifePremium premium = new TermLifePremium(
+		Premium premium = new Premium(
 				TermLifePremiumCalculator.getBasicAmount(quote),
 				TermLifePremiumCalculator.getPlusAmount(quote),
 				TermLifePremiumCalculator.getUltraAmount(quote));
@@ -23,7 +21,7 @@ public class SimulatorServiceImpl implements SimulatorService{
 	@Override
 	public Premium getWholeLifePremium(Quote quote) {
 		
-		WholeLifePremium premium = new WholeLifePremium(
+		Premium premium = new Premium(
 				WholeLifePremiumCalculator.getBasicAmount(quote),
 				WholeLifePremiumCalculator.getPlusAmount(quote),
 				WholeLifePremiumCalculator.getUltraAmount(quote));
