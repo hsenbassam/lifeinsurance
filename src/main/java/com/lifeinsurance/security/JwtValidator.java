0c0d2092a90fda1d89d2e856ed50a1d2e771b24d
hsenbassam <husseinbassam95@gmail.com>
2018-04-18 12:15:08 +0300

    Adding JWT Expiration Date

--- src/main/java/com/lifeinsurance/security/JwtValidator.java
+++ src/main/java/com/lifeinsurance/security/JwtValidator.java
@@ -1,5 +1,6 @@
 package com.lifeinsurance.security;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +18,36 @@ public class JwtValidator {
 	@Autowired
 	byte[] signingKey;
 
+	private Claims body;
+
 	@SuppressWarnings("unchecked")
 	public JwtUser validate(String token) {
 
 		JwtUser jwtUser = null;
 		try {
-			Claims body = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody();
+			body = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody();
 
 			jwtUser = new JwtUser();
 			jwtUser.setEmail(body.getSubject());
 			jwtUser.setId(Long.parseLong((String) body.get("id")));
 			jwtUser.setRole((List<String>) body.get("role"));
 		} catch (Exception e) {
-			throw new JwtAuthenticationException("JWT is incorrect",e);	
+			throw new JwtAuthenticationException("JWT is incorrect", e);
+		}
+		if (this.isTokenExpired()) {
+			throw new JwtAuthenticationException("JWT is Expired ");
 		}
 
 		return jwtUser;
 	}
+
+	private Date generateCurrentDate() {
+		return new Date(System.currentTimeMillis());
+	}
+
+	private Boolean isTokenExpired() {
+		final Date expiration = body.getExpiration();
+		return expiration.before(this.generateCurrentDate());
+	}
+
 }
