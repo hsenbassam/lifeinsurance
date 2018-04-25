17fcd7a8c4f1e8fae26dcafe2c7c43ee3bbc41e6
hsenbassam <husseinbassam95@gmail.com>
2018-04-25 10:52:48 +0300

    Fixing Java Routes + Not Found + No Access

--- src/main/java/com/lifeinsurance/controller/HomeController.java
+++ src/main/java/com/lifeinsurance/controller/HomeController.java
@@ -8,19 +8,10 @@ public class HomeController {
 
 	public static final String INDEX_PAGE = "index.html";
 
-	@GetMapping(value = { "/", "/products", "/profile", "/shopping-cart", "/login", "/register", "/checkout","/about" })
+	@GetMapping("/")
 	public String showHome() {
 
 		return INDEX_PAGE;
 
 	}
-
-
-	@GetMapping(value = { "/products/**", "/profile/**", "/quote/**", "/admin/**", "/payment/**" })
-	public String redirectHome() {
-
-		return "redirect:/" + INDEX_PAGE;
-
-	}
-
 }
